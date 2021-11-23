f7e910f4d19ccd150d83886d18e9b9e0e7bffce2
Maianne Thornton <maiannethornton@Maiannes-MBP.lan>
2021-11-23 14:13:48 -0500

    Imported data from data.js, added functionality of buttons, and added toggle for displaying info. Initial version of project is complete.

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  // state value for toggling - default set to hide info
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        {/* when clicked toggles showInfo state */}
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {/* ternary: if showInfo is true display - icon if showInfo is false display + icon */}
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {/* displaying the p depending on whether toggled on or off */}
+      {showInfo && <p>{info}</p>}
+
+    </article>
+  );
 };
 
 export default Question;
